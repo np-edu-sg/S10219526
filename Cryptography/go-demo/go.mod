49ed20cfe7806b0d1d6e8e738aa9c36c4a8d9d20
Qin Guan <qinguan20040914@gmail.com>
2022-01-25 16:46:03 +0800

    [Cryptography/go-demo] Added project

---
+++ Cryptography/go-demo/go.mod
@@ -0,0 +1,25 @@
+module go-demo
+
+go 1.17
+
+require (
+	github.com/binance-chain/tss-lib v1.3.3
+	github.com/cloudflare/redoctober v0.0.0-20211013234631-6a74ccc611f6
+	golang.org/x/crypto v0.0.0-20220112180741-5e0467b6c7ce
+)
+
+require (
+	github.com/agl/ed25519 v0.0.0-20170116200512-5312a6153412 // indirect
+	github.com/btcsuite/btcd v0.0.0-20190629003639-c26ffa870fd8 // indirect
+	github.com/decred/dcrd/dcrec/edwards/v2 v2.0.0 // indirect
+	github.com/gogo/protobuf v1.2.1 // indirect
+	github.com/ipfs/go-log v0.0.1 // indirect
+	github.com/mattn/go-colorable v0.1.2 // indirect
+	github.com/mattn/go-isatty v0.0.9 // indirect
+	github.com/opentracing/opentracing-go v1.1.0 // indirect
+	github.com/otiai10/primes v0.0.0-20180210170552-f6d2a1ba97c4 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/whyrusleeping/go-logging v0.0.0-20170515211332-0457bb6b88fc // indirect
+	golang.org/x/sys v0.0.0-20211013075003-97ac67df715c // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
+)
---
+++ Cryptography/go-demo/main.go
@@ -0,0 +1,185 @@
+package main
+
+import (
+	"crypto/cipher"
+	"crypto/rand"
+	"fmt"
+	"github.com/cloudflare/redoctober/padding"
+	"io"
+	"math/big"
+	"time"
+
+	"golang.org/x/crypto/curve25519"
+	"golang.org/x/crypto/twofish"
+
+	"github.com/binance-chain/tss-lib/crypto/paillier"
+)
+
+const (
+	paillierKeyLength = 1024
+)
+
+func waitEnter() {
+	fmt.Println("Press ENTER to continue...")
+	_, _ = fmt.Scanln()
+}
+
+func gen32ByteRandom() ([]byte, error) {
+	random := make([]byte, 32)
+	_, err := rand.Read(random)
+	if err != nil {
+		return nil, err
+	}
+
+	return random, err
+}
+
+// genCurve25519Keys returns a private key, public key, and error
+func genCurve25519Keys() ([]byte, []byte, error) {
+	// Generate randoms
+	private, err := gen32ByteRandom()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	pub, err := curve25519.X25519(private, curve25519.Basepoint)
+
+	return private, pub, err
+}
+
+// 1. Generate Alice and Bobs Paillier pub and priv key
+// 2. Generate Alice and Bobs curve25519 pub and priv key
+
+// 3. Alice and Bob exchanges curve25519 pub key with Paillier encryption. Assume each others Paillier public keys are known and secure
+// 4. Alice and Bob generate shared curve25519 secret
+// 5. Alice encrypts some data through Twofish with curve25519 secret
+// 6. Bob decrypts
+func main() {
+	fmt.Println("CTG Assignment Demonstration")
+	fmt.Println("NOTE: All values are displayed in hex")
+	fmt.Println()
+
+	alicePaillierPrivate, alicePaillierPub, err := paillier.GenerateKeyPair(paillierKeyLength, 10*time.Minute)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Alice Paillier Keys:\nPublic: %x\nPrivate: %x\n\n", alicePaillierPub.N, alicePaillierPrivate.LambdaN)
+
+	bobPaillierPrivate, bobPaillierPub, err := paillier.GenerateKeyPair(paillierKeyLength, 10*time.Minute)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Bob Paillier Keys:\nPublic: %x\nPrivate: %x\n\n", bobPaillierPub.N, bobPaillierPrivate.LambdaN)
+
+	aliceCurvePrivate, aliceCurvePub, err := genCurve25519Keys()
+	if err != nil {
+		panic(err)
+	}
+
+	waitEnter()
+
+	fmt.Printf("Alice Curve25519 Keys:\nPublic: %x\nPrivate: %x\n\n", aliceCurvePub, aliceCurvePrivate)
+
+	bobCurvePrivate, bobCurvePub, err := genCurve25519Keys()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Bob Curve25519 Keys:\nPublic: %x\nPrivate: %x\n\n", bobCurvePub, bobCurvePrivate)
+
+	e := new(big.Int)
+	e.SetBytes(aliceCurvePub)
+
+	aliceCurvePubEnc, err := bobPaillierPub.Encrypt(e)
+	if err != nil {
+		panic(err)
+	}
+
+	waitEnter()
+
+	fmt.Printf("Alice sends her Curve25519 Public key encrypted with Bob Paillier public key: %x\n\n", aliceCurvePubEnc)
+
+	aliceCurvePubDec, err := bobPaillierPrivate.Decrypt(aliceCurvePubEnc)
+
+	waitEnter()
+
+	fmt.Printf("Bob decrypts Alice Curve25519 Public key with his Paillier private key: %x\n\n", aliceCurvePubDec)
+
+	e = new(big.Int)
+	e.SetBytes(bobCurvePub)
+
+	bobCurvePubEnc, err := alicePaillierPub.Encrypt(e)
+	if err != nil {
+		panic(err)
+	}
+
+	waitEnter()
+
+	fmt.Printf("Bob sends his Curve25519 Public key encrypted with Alice Paillier public key: %x\n\n", bobCurvePubEnc)
+
+	bobCurvePubDec, err := alicePaillierPrivate.Decrypt(bobCurvePubEnc)
+
+	waitEnter()
+
+	fmt.Printf("Alice decrypts Bob Curve25519 Public key with her Paillier private key: %x\n\n", bobCurvePubDec)
+
+	aliceAgreed, err := curve25519.X25519(aliceCurvePrivate, bobCurvePubDec.Bytes())
+	if err != nil {
+		panic(err)
+	}
+
+	waitEnter()
+
+	fmt.Printf("Alice arrives at secret: %x\n\n", aliceAgreed)
+
+	bobAgreed, err := curve25519.X25519(bobCurvePrivate, aliceCurvePubDec.Bytes())
+	if err != nil {
+		panic(err)
+	}
+
+	waitEnter()
+
+	fmt.Printf("Bob arrives at secret: %x\n\n", bobAgreed)
+
+	fmt.Println("Alice now encrypts some data to send to Bob")
+
+	tfCipher, err := twofish.NewCipher(aliceAgreed)
+	if err != nil {
+		panic(err)
+	}
+
+	var input string
+	fmt.Print("Enter some data to encrypt: ")
+	_, err = fmt.Scanln(&input)
+	if err != nil {
+		panic(err)
+	}
+
+	paddedInput := padding.AddPadding([]byte(input))
+
+	ciphertext := make([]byte, len(paddedInput))
+	plaintext := make([]byte, len(paddedInput))
+
+	iv := make([]byte, twofish.BlockSize)
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		panic(err)
+	}
+
+	enc := cipher.NewCBCEncrypter(tfCipher, iv)
+	dec := cipher.NewCBCDecrypter(tfCipher, iv)
+
+	enc.CryptBlocks(ciphertext, paddedInput)
+
+	fmt.Printf("\nAlice encrypts data with shared secret:\nCiphertext: %x\n\n", ciphertext)
+
+	dec.CryptBlocks(plaintext, ciphertext)
+	pt, err := padding.RemovePadding(plaintext)
+	if err != nil {
+		panic(err)
+	}
+
+	waitEnter()
+	fmt.Printf("Bob decrypts data with shared secret:\nPlaintext: %s\n", pt)
+}
